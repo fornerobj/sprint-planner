049680982d6f0df9bfb85fd2e095daefdf353b31
fornerobj <fornerobj@gmail.com>
2025-03-29 12:01:11 -0500

    Added basic ui scaffold

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,58 @@
 import Link from "next/link";
 
 export default function HomePage() {
+  let id = 1;
+  const required = [
+    "Scaffold basic ui with mock data",
+    "Set up database",
+    "Routing/Post page",
+  ];
+  const in_progress = [
+    "Attach db to ui",
+    "Add auth",
+    "Add activity creation",
+    "Error management",
+  ];
+  const finished = [
+    "delete button (with server actions???)",
+    "Analytics?",
+    "Rate Limiting?",
+  ];
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      <h1>Hello (Spint Planner in Progress)</h1>
+    <main className="">
+      <div className="flex flex-col">
+        <header className="flex h-8 justify-around">
+          <h1>KANBAN</h1>
+          <p>Sign in</p>
+          <p>Home</p>
+        </header>
+        <div className="flex text-center">
+          <div className="flex h-full w-1/3 flex-col">
+            <h1>Required</h1>
+            {required.map((activity) => (
+              <div key={id++} className="bg-slate-500 p-4">
+                <h1>{activity}</h1>
+              </div>
+            ))}
+          </div>
+          <div className="flex h-full w-1/3 flex-col">
+            <h1>In Progress</h1>
+            {in_progress.map((activity) => (
+              <div key={id++} className="bg-slate-500 p-4">
+                <h1>{activity}</h1>
+              </div>
+            ))}
+          </div>
+          <div className="flex h-full w-1/3 flex-col">
+            <h1>Finished</h1>
+            {finished.map((activity) => (
+              <div key={id++} className="bg-slate-500 p-4">
+                <h1>{activity}</h1>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
     </main>
   );
 }
