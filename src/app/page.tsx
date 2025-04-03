518e43bc863e1be4f118f11e8df37c2bdab40f63
fornerobj <fornerobj@gmail.com>
2025-04-02 23:01:52 -0500

    maybe?

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,17 @@
 import { SignedIn, SignedOut } from "@clerk/nextjs";
 import { ProjectList } from "./_components/ProjectList";
 import { getProjectsByTeamMember } from "~/server/queries";
+import { auth } from "@clerk/nextjs/server";
+import type { Project } from "~/server/db/schema";
 
 export const dynamic = "force-dynamic";
 
 export default async function HomePage() {
-  const projects = await getProjectsByTeamMember();
+  const user = await auth();
+  const projects: Project[] = [];
+  if (user) {
+    const projects = await getProjectsByTeamMember();
+  }
   return (
     <main className="h-full p-8">
       <SignedOut>
